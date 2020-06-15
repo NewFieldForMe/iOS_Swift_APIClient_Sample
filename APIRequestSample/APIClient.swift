12d9b017269ab07a960b4556f2ee1c6f4919de62
RyoYamada <newfield1001@gmail.com>
2020-06-15 17:29:19 +0900

    first commit

---
+++ APIRequestSample/APIClient.swift
@@ -0,0 +1,13 @@
+//
+//  APIClient.swift
+//  APIRequestSample
+//
+//  Created by yamada.ryo on 2020/06/15.
+//  Copyright © 2020 yamada.ryo. All rights reserved.
+//
+
+import Foundation
+
+class WebAPIClient {
+
+}
---
+++ APIRequestSample/APIResponse.swift
@@ -0,0 +1,13 @@
+//
+//  APIResponse.swift
+//  APIRequestSample
+//
+//  Created by yamada.ryo on 2020/06/15.
+//  Copyright © 2020 yamada.ryo. All rights reserved.
+//
+
+import Foundation
+
+class APIResponse {
+    
+}
